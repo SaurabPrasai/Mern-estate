6d10dcc280bb99b5e5ed0c9dfd7faf7b59c99b16
Saurab Prasai <saurablovetocode@gmail.com>
2024-04-20 16:18:56 +0545

    complete signin ui and functionality

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,7 +1,79 @@
-import React from 'react'
+import { useState } from "react";
+import {useNavigate,Link} from "react-router-dom"
 
 export default function Signin() {
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(null);
+  const navigate=useNavigate();
+
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const res = await fetch("/api/auth/signin", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+
+      if (!res.ok) {
+        setError(data.message);
+        setLoading(false);
+        return;
+      }
+      setLoading(false);
+      setError(null)
+      navigate('/')
+    } catch (error) {
+      setLoading(false)
+      setError(error.message)
+    }
+  };
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
   return (
-    <div>Signin</div>
-  )
+    <div className=" p-3 max-w-lg mx-auto">
+      <h1 className=" text-3xl text-center font-semibold my-7">Sign in</h1>
+      <form className=" flex flex-col gap-4 " onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="email"
+          className=" border p-3 rounded-lg"
+          id="email"
+          onChange={handleChange}
+          required
+        />
+
+        <input
+          type="password"
+          placeholder="password"
+          className=" border p-3 rounded-lg"
+          id="password"
+          onChange={handleChange}
+          required
+        />
+        <button
+          className=" bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+          disabled={loading}
+        >
+          {loading ? "Loading..." : " Sign in"}
+        </button>
+      </form>
+      <div className=" flex gap-2 mt-5">
+        <p>Don't have an account?</p>
+        <Link to={"/sign-up"}>
+          <span className=" text-blue-700">Sign up</span>
+        </Link>
+      </div>
+      {error && <p className=" text-red-500 mt-5">{error}</p>}
+    </div>
+  );
 }
+
