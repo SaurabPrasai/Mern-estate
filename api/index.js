487ef758ead7fe717f48f3c778e59b00ead481a0
Saurab Prasai <saurablovetocode@gmail.com>
2024-04-20 10:55:42 +0545

    create database connection

--- api/index.js
+++ api/index.js
@@ -1,8 +1,27 @@
 import express from "express"
+import  userRouter from "./routes/user.route.js"
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+
+dotenv.config();
 
 const app=express();
 
+mongoose.connect(process.env.MONGO_URI)
+.then(()=>{
+    console.log("MongoDB Connected");
+})
+.catch((err)=>{
+    console.log(err);
+})
+
+
+
+
+// middleware
+app.use("/api/user",userRouter)
 
 app.listen(3000,()=>{
     console.log("Server is running on port 3000");
 })
+
