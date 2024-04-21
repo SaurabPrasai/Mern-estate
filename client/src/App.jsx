bb3d5101cf98a585518b62ccd53e0f17600c0228
Saurab Prasai <saurablovetocode@gmail.com>
2024-04-21 21:34:34 +0545

    update header and make the profile page private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,8 +5,10 @@ import About from "./pages/About";
 import SignUp from "./pages/SignUp";
 import Profile from "./pages/Profile";
 import Header from "./components/Header";
+import { useSelector } from "react-redux";
 
 export default function App() {
+  const {currentUser}=useSelector(state=>state.user);
   return <BrowserRouter>
   <Header/>
   <Routes>
@@ -14,7 +16,7 @@ export default function App() {
     <Route path="/sign-in" element={<Signin/>} />
     <Route path="/sign-up" element={<SignUp/>} />
     <Route path="/about" element={<About/>} />
-    <Route path="/profile" element={<Profile/>} />
+    <Route path="/profile" element={currentUser?<Profile/>:<Signin/>} />
   </Routes>
 
   </BrowserRouter>;
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,9 @@
 import { FaSearch } from "react-icons/fa";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
 export default function Header() {
+  const { currentUser } = useSelector((state) => state.user);
   return (
     <header className=" bg-slate-200 shadow-md">
       <div className=" flex justify-between items-center max-w-6xl  mx-auto p-3">
@@ -35,11 +37,21 @@ export default function Header() {
               About
             </li>
           </Link>
-          <Link to={"/sign-in"}>
-            <li className="sm:inline text-slate-700 hover:underline">
-              Sign in
-            </li>
-          </Link>
+          {currentUser ? (
+            <Link to={"/profile"}>
+              <img
+                src={currentUser.avatar}
+                alt="profile"
+                className=" rounded-full h-7 w-7 object-cover"
+              />
+            </Link>
+          ) : (
+            <Link to={"/sign-in"}>
+              <li className="sm:inline text-slate-700 hover:underline">
+                Sign in
+              </li>
+            </Link>
+          )}
         </ul>
       </div>
     </header>
